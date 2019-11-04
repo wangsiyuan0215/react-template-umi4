5893bb8712ff5a862f6d7a7fac944ac42bddbd58
siyuan.wang <siyuan0215@gmail.com>
2019-11-04 17:06:07 +0800

    [feature] Added complexity rule in eslint.

--- .eslintrc.js
+++ .eslintrc.js
@@ -11,13 +11,14 @@ module.exports = {
     },
     globals: {},
     rules: {
-        'react/jsx-filename-extension': [1, { extensions: ['.js'] }],
-        'react/jsx-wrap-multilines': 0,
-        'react/prop-types': 0,
-        'react/forbid-prop-types': 0,
-        'react/jsx-one-expression-per-line': 0,
         'react/jsx-indent': [4, 'always'],
+        'react/prop-types': 0,
         'react/jsx-indent-props': [4, 'always'],
+        'react/forbid-prop-types': 0,
+        'react/jsx-wrap-multilines': 0,
+        'react/jsx-filename-extension': [1, { extensions: ['.js'] }],
+        'react/jsx-one-expression-per-line': 0,
+
         'import/no-unresolved': [2, { ignore: ['^@/', '^umi/', '^root/'] }],
         'import/no-extraneous-dependencies': [
             2,
@@ -26,10 +27,13 @@ module.exports = {
                 devDependencies: ['**/tests/**.js', '/mock/**.js', '**/**.test.js']
             }
         ],
-        'jsx-a11y/no-noninteractive-element-interactions': 0,
+
+        'jsx-a11y/anchor-is-valid': 0,
         'jsx-a11y/click-events-have-key-events': 0,
         'jsx-a11y/no-static-element-interactions': 0,
-        'jsx-a11y/anchor-is-valid': 0,
+        'jsx-a11y/no-noninteractive-element-interactions': 0,
+
+        "complexity": ["error", 8],
         'linebreak-style': 0
     },
     settings: {
